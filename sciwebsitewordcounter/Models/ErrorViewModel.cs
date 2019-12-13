d0842b310b596421273efaa7b4a387169ae389c9
Joshua Mangi <joshuamangi@gmai.com>
2019-12-13 21:05:33 +0300

    Add project files.

---
+++ sciwebsitewordcounter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sciwebsitewordcounter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
